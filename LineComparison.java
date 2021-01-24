fb3b1c4db33518fb9523bf51ae15afa9935b3307
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-01-24 14:47:28 -0800

    Comparison of the length of line 4th part done

--- LineComparison.java
+++ LineComparison.java
@@ -1,7 +1,19 @@
 class LineComparison
 {
    int LENGTH_OF_1ST_LINE,LENGTH_OF_2ND_LINE;
-   public void compareTo(int x1,int x2,int y1,int y2,int a1,int a2,int b1,int b2)
+	int x1, x2, y1, y2, a1, a2, b1, b2;
+	public LineComparison(int x1,int x2,int y1,int y2,int a1,int a2,int b1,int b2)
+	{
+		this.x1=x1;
+		this.x2=x2;
+		this.y1=y1;
+		this.y2=y2;
+		this.a1=a1;
+		this.a2=a2;
+		this.b1=b1;
+		this.b2=b2;
+	}
+   public void compareTo()
    {
       LENGTH_OF_1ST_LINE=(int) (Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1))));
       LENGTH_OF_2ND_LINE=(int) (Math.sqrt(((a2-a1)*(a2-a1))+((b2-b1)*(b2-b1))));
@@ -14,8 +26,7 @@ class LineComparison
    }
    public static void main(String args[])
    {
-      System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
-      LineComparison obj1=new LineComparison();
-      obj1.compareTo(6,9,2,6,6,9,2,1);
+      LineComparison obj1=new LineComparison(6,9,2,6,6,9,2,1);
+      obj1.compareTo();
    }
 }
